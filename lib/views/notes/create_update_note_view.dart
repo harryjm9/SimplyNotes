ef0c8b4daadc7e8cbd0c6e3341c9034d84efd3f5
Harry Mackay <harryjmackay@hotmail.com>
2023-01-31 12:28:13 +1300

    End of chapter 38

--- lib/views/notes/create_update_note_view.dart
+++ lib/views/notes/create_update_note_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:simply_notes/services/auth/auth_service.dart';
 import 'package:simply_notes/utilities/generics/get_arguments.dart';
+import 'package:simply_notes/utilities/dialogs/cannot_share_empty_note_dialog.dart';
 import 'package:simply_notes/services/cloud/cloud_note.dart';
 import 'package:simply_notes/services/cloud/cloud_storage_exceptions.dart';
 import 'package:simply_notes/services/cloud/firebase_cloud_storage.dart';
@@ -75,6 +77,16 @@ class _CreateUpdateNoteViewState extends State<CreateUpdateNoteView> {
       appBar: AppBar(
         title: const Text('New Note'),
         actions: [
+          IconButton(
+              onPressed: () async {
+                final text = _textController.text;
+                if (_note == null || text.isEmpty) {
+                  await showCannotShareEmptyNoteDialog(context);
+                } else {
+                  Share.share(text);
+                }
+              },
+              icon: const Icon(Icons.share)),
           IconButton(
             onPressed: () {
               _saveNoteIfTextNotEmpty();
